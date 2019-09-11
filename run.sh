fc09b9d915cb706b0c39578c0418948de128228a
PK <pradhyu.shrestha@gmail.com>
2019-09-10 22:19:29 -0400

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 cd fluentd
-./build.sh
+docker images | grep  fluent/fluentd:latest 
+if [ "$?" = "0" ]; then
+    echo "do nothing"
+else
+    ./build.sh
+fi
+
+
+
 cd ..
 docker-compose -f docker-compose.yml up -d
 docker-compose -f docker-compose.yml logs -f
